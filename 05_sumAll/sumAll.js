3ef2264703c1fbb68ee46b03949005f9abaa9020
ali-hasan97 <ali_hasan97@hotmail.com>
2022-08-06 06:04:44 -0500

    finished exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (typeof(start) && typeof(end) != 'number') {
+        return 'ERROR';
+    }
+    if (start < 0 == true || end < 0 == true) {
+        return 'ERROR';
+    }
+    if (start > end) {
+        [start, end] = [end, start];
+    }
+    let result = 0n;
+        for (let i = BigInt(start); i <= BigInt(end); i++) {
+            result += i;
+        }
+    result = Number(result);
+    return result;
 };
 
 // Do not edit below this line
