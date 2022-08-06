6fe51da26eec9b85aafbcb62eb885f8104ca82e1
ali-hasan97 <ali_hasan97@hotmail.com>
2022-08-06 04:48:09 -0500

    finished exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR'
+    }
+    let final = ''
+    for (let x=0; x < num; x++) {
+        final += string;
+    }
+    return final;
 };
 
 // Do not edit below this line
