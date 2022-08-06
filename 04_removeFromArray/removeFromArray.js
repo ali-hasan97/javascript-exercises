e40930b7c083ba7c9709f2e5172a0b9af758ed7c
ali-hasan97 <ali_hasan97@hotmail.com>
2022-08-06 05:33:36 -0500

    finished exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...remove) {
+    // make sure your comparitors are === to account for type
+    for (let x = 0; x < array.length; x++) {
+        for (let i = 0; i < remove.length; i++) {
+            if (array[x] === remove[i]) {
+                array.splice(x, 1);
+                x--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
