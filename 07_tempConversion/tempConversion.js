f6bdc993a2ebd8b936d253d8e97ae098a59a6f15
ali-hasan97 <ali_hasan97@hotmail.com>
2022-08-06 06:24:03 -0500

    finished exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(fahr) {
+  //round to one decimal
+  let celsius = (fahr-32)*5/9;
+  let temp = celsius.toFixed(1);
+  celsius = Number(temp)
+  return celsius
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  //round to one decimal
+  let fahrenheit = (cel*9/5) + 32;
+  let temp = fahrenheit.toFixed(1);
+  fahrenheit = Number(temp)
+  return fahrenheit
 };
 
 // Do not edit below this line
